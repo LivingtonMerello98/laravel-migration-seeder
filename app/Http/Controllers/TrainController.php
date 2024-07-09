d162f64df21ced84aaa674cc6e77ab61347346d8
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-09 16:40:03 +0200

    [upd] TrainController class definition with the index

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
+
+//utilizzo della libreria caebon
+use Carbon\Carbon;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $today = Carbon::today()->toDateString();
+        $trains = Train::whereDate('orario_partenza', $today)->get();
+
+        return view('trains.index', compact('trains'));
+    }
 }
