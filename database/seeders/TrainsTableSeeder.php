bf76e4f5859732e0d2a46ef94ebd2da94af65f00
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-10 16:35:27 +0200

    [upd] TrainsTableSeeder set

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Factory as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,5 +14,20 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
+
+        for ($i = 0; 0 <= 50; $i++) {
+            Train::create([
+                'azienda' => $faker->company,
+                'stazione_partenza' => $faker->city,
+                'data_partenza' => $faker->date('Y_m_d'),
+                'stazione_arrivo' => $faker->city,
+                'orario_arrivo' => $faker->time,
+                'codice_treno' => $faker->numerify('#####'),
+                'numero_carrozze' => $faker->numberBetween(1, 15),
+                'in_orario' => $faker->boolean,
+                'cancellato' => $faker->boolean,
+            ]);
+        }
     }
 }
